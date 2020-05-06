6c1910476e6ff6c009aa5e27a3605b782cd871ca
souj <soujanya.venkatesh@gmail.com>
2020-05-06 23:03:42 +0530

    Deploy

--- src/api/user/model.js
+++ src/api/user/model.js
@@ -44,18 +44,18 @@ const userSchema = new Schema({
   timestamps: true
 })
 
-userSchema.path('email').set(function (email) {
-  if (!this.picture || this.picture.indexOf('https://gravatar.com') === 0) {
-    const hash = crypto.createHash('md5').update(email).digest('hex')
-    this.picture = `https://gravatar.com/avatar/${hash}?d=identicon`
-  }
-
-  if (!this.name) {
-    this.name = email.replace(/^(.+)@.+$/, '$1')
-  }
-
-  return email
-})
+// userSchema.path('email').set(function (email) {
+//   if (!this.picture || this.picture.indexOf('https://gravatar.com') === 0) {
+//     const hash = crypto.createHash('md5').update(email).digest('hex')
+//     this.picture = `https://gravatar.com/avatar/${hash}?d=identicon`
+//   }
+//
+//   if (!this.name) {
+//     this.name = email.replace(/^(.+)@.+$/, '$1')
+//   }
+//
+//   return email
+// })
 
 userSchema.pre('save', function (next) {
   if (!this.isModified('password')) return next()
