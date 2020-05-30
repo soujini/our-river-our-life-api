6e44d21446afccb3e08f7ad389531796ca911ec5
souj <soujanya.venkatesh@gmail.com>
2020-05-30 18:46:57 +0530

    login

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -3,8 +3,25 @@ import { success } from '../../services/response/'
 
 export const login = ({ user }, res, next) =>{
 console.log(user)
-  sign(user.id)
+  sign(user.phoneNumber)
     .then((token) => ({ token, user: user.view(true) }))
     .then(success(res, 201))
     .catch(next)
   }
+
+  export const create = ({ bodymen: { body } }, res, next) =>
+    Test.create(body)
+      .then((test) => test.view(true))
+      .then(success(res, 201))
+      .catch((err) => {
+        /* istanbul ignore else */
+        if (err.name === 'MongoError' && err.code === 11000) {
+          res.status(409).json({
+            valid: false,
+            param: 'phone',
+            message: 'Phone number already registered'
+          })
+        } else {
+          next(err)
+        }
+      })
