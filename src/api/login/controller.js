940c12652e8fff3080ad39febefc6d8092a082d4
souj <soujanya.venkatesh@gmail.com>
2020-05-30 14:51:58 +0530

    login

--- src/api/login/controller.js
+++ src/api/login/controller.js
@@ -4,7 +4,7 @@ import { Login } from '.'
 export const login = ({ bodymen: { body } }, res, next) =>{
 Login.findOneAndUpdate({phoneNumber:body.phoneNumber},{phoneNumber:body.phoneNumber},{new: true, upsert: true })
 .then((login) => ({
-  login.view(true)
+  login.view(true),
   console.log("Asdasd")
 })
 .then(success(res, 201))
