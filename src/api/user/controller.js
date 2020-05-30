efa198a8c5cffe80be7bf8b6d4b4bfc8043bfe28
souj <soujanya.venkatesh@gmail.com>
2020-05-30 20:49:49 +0530

    login

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -1,21 +1,6 @@
 import { success, notFound } from '../../services/response/'
 import { User } from '.'
 
-export const login = ({ bodymen: { body } }, res, next) =>{
-  console.log("souj")
-    console.log(body)
-  const a = body.id.split("[String: ')");
-  const b = a[0].split("]'");
-  console.log(b[0])
-const userId = b[0].toString();
-
-User.find(user => { user.id === userId })
-.then((user) => (user.view(true)))
-.then(success(res, 201))
-.catch(next)
-}
-
-
 export const create = ({ bodymen: { body } }, res, next) =>
 User.findOneAndUpdate({phoneNumber:body.phoneNumber},{phoneNumber:body.phoneNumber},{new: true, upsert: true })
 .then((user) => (user.view(true)))
