e912684058b1968aaf9fd4a5ce216d420e83ff1c
souj <soujanya.venkatesh@gmail.com>
2020-06-02 13:12:52 +0530

    login

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -2,51 +2,66 @@ import { success, notFound } from '../../services/response/'
 import { User } from '.'
 
 var ObjectId = require('mongodb').ObjectId
+const jwt = require('jsonwebtoken');
 
+const accessTokenSecret = 'youraccesstokensecret';
 
 export const login = ({ bodymen: { body } }, res, next) =>{
   console.log("souj")
-console.log(ObjectId(body.userId))
+  console.log(ObjectId(body.userId))
+  // Filter user from the users array by username and password
+  const user = User.find(u => { return u._id === ObjectId(body.userId) });
+  if (user) {
+    // Generate an access token
+    const accessToken = jwt.sign({ _id:ObjectId(body.userId) }, accessTokenSecret);
+console.log("access token")
+console.log(accessToken)
+    res.json({
+      accessToken
+    });
+  } else {
+    res.send('User Id is incorrect');
+  }
 
 }
 
 export const signIn = ({ bodymen: { body } }, res, next) =>{
-console.log(body)
-User.findOneAndUpdate({phoneNumber:body.phoneNumber},{phoneNumber:body.phoneNumber},{new: true, upsert: true })
-.then((user) => (user.view(true)))
-.then(success(res, 201))
-.catch(next)
+  console.log(body)
+  User.findOneAndUpdate({phoneNumber:body.phoneNumber},{phoneNumber:body.phoneNumber},{new: true, upsert: true })
+  .then((user) => (user.view(true)))
+  .then(success(res, 201))
+  .catch(next)
 }
 
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
-  User.count(query)
-    .then(count => User.find(query, select, cursor)
-      .then((users) => ({
-        count,
-        rows: users.map((user) => user.view())
-      }))
-    )
-    .then(success(res))
-    .catch(next)
+User.count(query)
+.then(count => User.find(query, select, cursor)
+.then((users) => ({
+  count,
+  rows: users.map((user) => user.view())
+}))
+)
+.then(success(res))
+.catch(next)
 
 export const show = ({ params }, res, next) =>
-  User.findById(params.id)
-    .then(notFound(res))
-    .then((user) => user ? user.view() : null)
-    .then(success(res))
-    .catch(next)
+User.findById(params.id)
+.then(notFound(res))
+.then((user) => user ? user.view() : null)
+.then(success(res))
+.catch(next)
 
 export const update = ({ bodymen: { body }, params }, res, next) =>
-  User.findById(params.id)
-    .then(notFound(res))
-    .then((user) => user ? Object.assign(user, body).save() : null)
-    .then((user) => user ? user.view(true) : null)
-    .then(success(res))
-    .catch(next)
+User.findById(params.id)
+.then(notFound(res))
+.then((user) => user ? Object.assign(user, body).save() : null)
+.then((user) => user ? user.view(true) : null)
+.then(success(res))
+.catch(next)
 
 export const destroy = ({ params }, res, next) =>
-  User.findById(params.id)
-    .then(notFound(res))
-    .then((user) => user ? user.remove() : null)
-    .then(success(res, 204))
-    .catch(next)
+User.findById(params.id)
+.then(notFound(res))
+.then((user) => user ? user.remove() : null)
+.then(success(res, 204))
+.catch(next)
