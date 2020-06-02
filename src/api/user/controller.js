aa1093788a16ecbaf990757d7cee5e16765fb0ac
souj <soujanya.venkatesh@gmail.com>
2020-06-02 13:19:23 +0530

    login

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -11,17 +11,7 @@ export const login = ({ bodymen: { body } }, res, next) =>{
   console.log(ObjectId(body.userId))
   // Filter user from the users array by username and password
   const user = User.find(u => { return u.id === ObjectId(body.userId) });
-  if (user) {
-    // Generate an access token
-    const accessToken = jwt.sign({ id:ObjectId(body.userId) }, accessTokenSecret);
-console.log("access token")
-console.log(accessToken)
-    res.json({
-      accessToken
-    });
-  } else {
-    res.send('User Id is incorrect');
-  }
+console.log(user.id)
 
 }
 
