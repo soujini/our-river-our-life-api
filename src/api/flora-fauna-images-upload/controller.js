064b531bc2d9bd61c7fbfb931a077305a2b83aaa
souj <soujanya.venkatesh@gmail.com>
2020-06-03 15:02:59 +0530

    Images Upload

--- src/api/flora-fauna-images-upload/controller.js
+++ src/api/flora-fauna-images-upload/controller.js
@@ -30,8 +30,8 @@ export const upload = (req, res, next) =>{
       const locationUrl = data.Location;
       console.log(locationUrl);
       var params ={"id":"5ed5cd1e1177d200176877a6"}
-      var body = {"flora":locationUrl}
-      WaterTestDetailsController.update({body, params})
+      var req = {"flora":locationUrl}
+      WaterTestDetailsController.update({rew, params})
     }
   });
 }
