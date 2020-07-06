62f024ae640fc4f19f31f44879a3cc93eb168cc0
souj <soujanya.venkatesh@gmail.com>
2020-07-06 19:54:21 +0530

    pdf.html

--- src/api/pdf/controller.js
+++ src/api/pdf/controller.js
@@ -36,7 +36,7 @@ let students = [
 
 export const generateReport = ({ body }, res, next) =>{
   console.log("in report");
-  ejs.renderFile(__dirname +'/report-template.ejs'), {
+  ejs.renderFile( (__dirname +'/report-template.ejs'), {
         students: students
     }, (err, data) => {
         if (err) {
@@ -63,7 +63,7 @@ export const generateReport = ({ body }, res, next) =>{
                   console.log("souj4");
                     res.send("File created successfully");
                 }
-            })
+            });
         }
     });
   // res.status(201).json(body);
