b15764124216f1d9dc97bcfbe4d8a694d60cab3a
souj <soujanya.venkatesh@gmail.com>
2020-07-06 19:46:14 +0530

    pdf.html

--- src/api/pdf/controller.js
+++ src/api/pdf/controller.js
@@ -36,14 +36,14 @@ let students = [
 
 export const generateReport = ({ body }, res, next) =>{
   console.log("in report");
-  ejs.renderFile(path.join(__dirname, '/report-template.ejs'), {
+  ejs.renderFile(__dirname +'/report-template.ejs'), {
         students: students
     }, (err, data) => {
         if (err) {
-          console.log("souj1");
+          console.log("souj1 ");
             res.send(err);
         } else {
-          console.log("souj2");
+          console.log("souj2 "+data);
             let options = {
                 "height": "11.25in",
                 "width": "8.5in",
@@ -55,7 +55,7 @@ export const generateReport = ({ body }, res, next) =>{
                 },
 
             };
-            pdf.create(data, options).toFile("report.pdf", function (err, data) {
+            pdf.create(data, options).toFile('kirti.pdf', function (err, data) {
                 if (err) {
                   console.log("souj3");
                     res.send(err);
