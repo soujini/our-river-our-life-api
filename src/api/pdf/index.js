621e48cdbb935eba0d09f28ca3eb6835989ec60f
souj <soujanya.venkatesh@gmail.com>
2020-07-07 12:56:12 +0530

    EJS

--- src/api/pdf/index.js
+++ src/api/pdf/index.js
@@ -10,6 +10,29 @@ let path = require("path");
 
 const router = new Router()
 
+let students = [
+  {name: "Joy",
+  email: "joy@example.com",
+  city: "New York",
+  country: "USA"},
+  {name: "John",
+  email: "John@example.com",
+  city: "San Francisco",
+  country: "USA"},
+  {name: "Clark",
+  email: "Clark@example.com",
+  city: "Seattle",
+  country: "USA"},
+  {name: "Watson",
+  email: "Watson@example.com",
+  city: "Boston",
+  country: "USA"},
+  {name: "Tony",
+  email: "Tony@example.com",
+  city: "Los Angels",
+  country: "USA"
+}];
+
 router.get("/generateReport", (req, res) => {
 	ejs.renderFile(path.join(__dirname, "/report-template.ejs"), {
         students: students
