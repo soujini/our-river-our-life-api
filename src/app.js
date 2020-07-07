0367149cb8da5851777520bf80b111d8f8632133
souj <soujanya.venkatesh@gmail.com>
2020-07-07 19:11:26 +0530

    Certificate

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express(apiRoot, api)
 const server = http.createServer(app)
 // app.use(express.static(path.join(__dirname, '/public')));
 // app.use('/static', express.static('public'))
-app.use(path.join(__dirname, '/public'))
+app.use(express.static(path.join(__dirname, '/public')))
 
 if (mongo.uri) {
   mongoose.connect(mongo.uri)
