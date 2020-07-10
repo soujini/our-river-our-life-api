488596e32145d587f1a2408e639cc3cc23e44978
souj <soujanya.venkatesh@gmail.com>
2020-07-10 20:01:31 +0530

    Certificate

--- src/api/water-test-details/controller.js
+++ src/api/water-test-details/controller.js
@@ -5,10 +5,10 @@ var PDFController = require('../pdf/controller')
 
 export const create = ({ bodymen: { body } }, res, next) =>{
   WaterTestDetails.create(body)
-  .then((waterTestDetails) =>{
-    PDFController.generateReport({waterTestDetails})
+  .then((waterTestDetails) =>({
+    PDFController.generateReport({waterTestDetails}),
     waterTestDetails.view(true)
-  })
+  }))
   .then(success(res, 201))
   .catch(next)
 }
