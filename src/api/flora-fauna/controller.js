4a8af731d14565bc9ce73e7fd0d0ab48564b0906
souj <soujanya.venkatesh@gmail.com>
2020-09-22 13:41:48 +0530

    Flora Fauna

--- src/api/flora-fauna/controller.js
+++ src/api/flora-fauna/controller.js
@@ -4,8 +4,6 @@ import aws from 'aws-sdk';
 import fs from 'fs';
 
 export const createFlora = (req, res, next) =>{
-  console.log("souji");
-  console.log(req);
   var customOriginalName="";
   var customPath="";
   var customFieldName="";
