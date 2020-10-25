0a0b8f40e6de9be949e0937a80661ff00c65c5f8
souj <soujanya.venkatesh@gmail.com>
2020-10-26 00:50:25 +0530

    Contributor Name

--- src/api/flora-fauna/controller.js
+++ src/api/flora-fauna/controller.js
@@ -172,6 +172,8 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
       .then((floraFaunas) => ({
         count,
         rows: floraFaunas.map((floraFauna) => {
+          console.log("ffff");
+          console.log(floraFauna);
           var user = UserController.getUser(floraFauna.userId);
           console.log(user);
           floraFauna.contributorName = user.firstName + ' ' +user.lastName;
