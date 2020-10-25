124dfaa1204bddc98d51a0d0255f7d9472d1690a
souj <soujanya.venkatesh@gmail.com>
2020-10-25 23:53:43 +0530

    Get Flora and Fauna - User Id Implementation

--- src/api/flora-fauna/controller.js
+++ src/api/flora-fauna/controller.js
@@ -171,7 +171,7 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
       .then((floraFaunas) => ({
         count,
         rows: floraFaunas.map((floraFauna) =>
-          floraFauna.contributorName = "Souji"
+          floraFauna.contributorName = "Souji",
           floraFauna.view())
       }))
     )
