2ce8df16cfd0beea560e487803697947ed0fc400
souj <soujanya.venkatesh@gmail.com>
2020-10-26 12:09:09 +0530

    Contributor Name

--- src/api/flora-fauna/controller.js
+++ src/api/flora-fauna/controller.js
@@ -172,7 +172,7 @@ export const index =  async ({ querymen: { query, select, cursor } }, res, next)
       .then((floraFaunas) => ({
         count,
         // rows: getUser(floraFaunas)
-        rows: floraFaunas.map((floraFauna) => {
+        rows: async floraFaunas.map((floraFauna) => {
           var params = {"userId":floraFauna['userId']}
           var userId  = floraFauna['userId'];
           //var user = await UserController.getUser({params});
