273ca8dbb31fa5867d6b9efdc786375c1eb25b96
souj <soujanya.venkatesh@gmail.com>
2020-10-26 13:23:43 +0530

    Contributor Name

--- src/api/water-test-details/controller.js
+++ src/api/water-test-details/controller.js
@@ -9,34 +9,34 @@ WaterTestDetails.create(body)
 .then(success(res, 201))
 .catch(next)
 
-// export const index = ({ querymen: { query, select, cursor } }, res, next) =>{
-// WaterTestDetails.count(query)
-// .then(count => WaterTestDetails.find(query, select, cursor)
-// .then((waterTestDetails) => ({
-//   count,
-//   rows: waterTestDetails.map((waterTestDetails) => waterTestDetails.view())
-// }))
-// )
-// .then(success(res))
-// .catch(next)
-// }
+export const index = ({ querymen: { query, select, cursor } }, res, next) =>{
+WaterTestDetails.count(query)
+.then(count => WaterTestDetails.find(query, select, cursor)
+.then((waterTestDetails) => ({
+  count,
+  rows: waterTestDetails.map((waterTestDetails) => waterTestDetails.view())
+}))
+)
+.then(success(res))
+.catch(next)
+}
 
-export const index = async ({ querymen: { query, select, cursor } }, res, next) =>{
-  WaterTestDetails.count(query)
-    .then(count => WaterTestDetails.find(query, select, cursor)
-      .then(async(waterTestDetails) => ({
-        count,
-         rows:  await Promise.all(waterTestDetails.map(async(waterTestDetails) => {
-          var params = {"userId":waterTestDetails['userId']}
-          var user = await UserController.getUser({params});
-          waterTestDetails.contributorName = user.firstName ? user.firstName : waterTestDetails['generalInformation']['testerName'] + ' ' +user.lastName ? user.lastName :'';
-          return waterTestDetails.view();
-        }))
-      }))
-    )
-    .then(success(res))
-    .catch(next)
-  }
+// export const index = async ({ querymen: { query, select, cursor } }, res, next) =>{
+//   WaterTestDetails.count(query)
+//     .then(count => WaterTestDetails.find(query, select, cursor)
+//       .then(async(waterTestDetails) => ({
+//         count,
+//          rows:  await Promise.all(waterTestDetails.map(async(waterTestDetails) => {
+//           var params = {"userId":waterTestDetails['userId']}
+//           var user = await UserController.getUser({params});
+//           waterTestDetails.contributorName = user.firstName ? user.firstName : waterTestDetails['generalInformation']['testerName'] + ' ' +user.lastName ? user.lastName :'';
+//           return waterTestDetails.view();
+//         }))
+//       }))
+//     )
+//     .then(success(res))
+//     .catch(next)
+//   }
 
 export const show = ({ params }, res, next) =>{
 WaterTestDetails.findById(params.id)
