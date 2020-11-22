6dc0d08ac49b64d35aa2a9239e492abcd2274ded
souj <soujanya.venkatesh@gmail.com>
2020-11-22 13:12:43 +0530

    WTD

--- src/api/water-test-details/controller.js
+++ src/api/water-test-details/controller.js
@@ -357,10 +357,11 @@ export const uploadRiver = async function(req) {
           }
 
         });
+        console.log(river);
+     resolve(river);
 
       }));
-      console.log(river);
-   resolve(river);
+
     }
     else{
       resolve([]);
