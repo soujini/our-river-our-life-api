2d4092e8bbb051d7a1d32c77440630fc867e0b21
souj <soujanya.venkatesh@gmail.com>
2020-11-23 13:01:37 +0530

    WTD

--- src/api/water-test-details/controller.js
+++ src/api/water-test-details/controller.js
@@ -29,7 +29,7 @@ var UserController = require('../user/controller')
 //   });
 //
 
-export const uploadToS3 = function(params) {
+export const uploadToS3 = function({params}) {
   console.log(params);
   const s3 = new aws.S3();
   var responseData=[];
