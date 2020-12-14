aa77d06014db416fa151e10ae55e517c17699a59
souj <soujanya.venkatesh@gmail.com>
2020-12-14 10:25:18 +0530

    WTD

--- src/api/water-test-details/controller.js
+++ src/api/water-test-details/controller.js
@@ -338,9 +338,9 @@ export const createWaterTestDetails = async(req, res, next) =>{
     req.body.activity = results[4];
     req.body.river = results[5];
 
-    console.log(req.body);
-    console.log(typeof(req.body));
-    WaterTestDetails.create(req.body)
+    console.log(JSON.parse(JSON.stringify(req.body)));
+    console.log(typeof(JSON.parse(JSON.stringify(req.body))));
+    WaterTestDetails.create(JSON.parse(JSON.stringify(req.body)))
     .then((waterTestDetails) => waterTestDetails.view(true))
     .then(success(res, 201))
     .catch(next)
