2ad9169ef37638ec3dde5f924a244fe64d4790ea
soujanya.venkatesh@gmail.com <soujanya.venkatesh@gmail.com>
2021-09-06 19:57:40 +0530

    Create fish Sanctuary

--- src/api/fish-sanctuaries/controller.js
+++ src/api/fish-sanctuaries/controller.js
@@ -118,6 +118,9 @@ export const createFishSanctuary = async(req, res, next) =>{
   Promise.all([uploadSanctuaryPictures(req),uploadSpeciesPictures(req)])
     // , uploadFishInformation(req)
   .then(results => {
+
+    console.log('ASTI');
+    console.log(results);
     for(var i=0;i<results[0].length;i++){
       //if(req.body.locationDetails.sanctuaryPictures != undefined || req.body.locationDetails.sanctuaryPictures != null){
         console.log("SOUJ");
