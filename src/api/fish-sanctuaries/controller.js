3384c403d1e51faf4d46da7cdbaddfca559afc1b
soujanya.venkatesh@gmail.com <soujanya.venkatesh@gmail.com>
2022-08-25 10:57:39 +0530

    Update Fish Sanc

--- src/api/fish-sanctuaries/controller.js
+++ src/api/fish-sanctuaries/controller.js
@@ -164,12 +164,8 @@ export const createFishSanctuary = async (req, res, next) => {
 
 export const updateFishSanctuary = async (req, res, next) => {
 
-  if (req.body['locationDetails']['speciesPictures'] != undefined) {
-    req.body['locationDetails']['sanctuaryPictures'] = JSON.parse(req.body['locationDetails']['sanctuaryPictures']);
-  }
-  if (req.body['locationDetails']['speciesPictures'] != undefined) {
-    req.body['speciesPictures'] = JSON.parse(req.body['speciesPictures']);
-  }
+  req.body['locationDetails']['sanctuaryPictures'] = JSON.parse(req.body['locationDetails']['sanctuaryPictures']);
+  req.body['speciesPictures'] = JSON.parse(req.body['speciesPictures']);
 
   Promise.all([uploadSanctuaryPictures(req), uploadSpeciesPictures(req)])
     .then(results => {
