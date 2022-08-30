96589215c91f6a6b6759d0dbe100300f5425fd43
soujanya.venkatesh@gmail.com <soujanya.venkatesh@gmail.com>
2022-08-30 18:19:56 +0530

    Create water test - Pictures

--- src/api/water-test-details/controller.js
+++ src/api/water-test-details/controller.js
@@ -395,7 +395,7 @@ export const createWaterTestDetails = async (req, res, next) => {
       console.log("in promise")
       console.log(results[0])
       results[5].forEach((element, index) => {
-        req.riverPictures.forEach((element2, index2) => {
+        req.body.riverPictures.forEach((element2, index2) => {
           if (element.fileName == element2.imageURL)
             req.riverPictures[index2].imageURL = element.imageURL
 
