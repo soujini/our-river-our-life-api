208b427872dfb83754ed7111b38d136f4d9460cb
soujini <soujanya.venkatesh@gmail.com>
2023-06-27 19:02:52 +0530

    Update pdf controller

--- src/api/pdf/controller.js
+++ src/api/pdf/controller.js
@@ -1,3 +1,4 @@
+import { parseAsync } from '@babel/core'
 import aws from 'aws-sdk'
 import { logger } from 'handlebars'
 const ejs = require('ejs')
@@ -87,7 +88,7 @@ export const generateReport = async (req, res, next) => {
   console.log('Water Test Details: ' + JSON.stringify(content.generalInformation))
   ejs.renderFile(path.join(__dirname, '/report-template.ejs'), {
     waterTestDetails: content
-  }, (err, data) => {
+  }, async (err, data) => {
     if (err) {
       res.send('Error in report template ' + err)
     } else {
@@ -128,7 +129,7 @@ export const generateReport = async (req, res, next) => {
             ContentType: 'application/pdf'
           }
 
-          s3.upload(params, async function (err, data) {
+          s3.upload(params, function (err, data) {
             if (err) {
               console.log(err)
               console.log('Error uploading data: ', data)
@@ -137,7 +138,7 @@ export const generateReport = async (req, res, next) => {
               console.log('Succesfully uploaded pdf!')
               var url = 'https://our-river-our-life-images.s3.amazonaws.com/certificate/certificate_' + waterTestDetailsId
               params = { id: req.body.id, certificate: url, fieldName: 'certificate' }
-              await WaterTestDetailsController.updateImage({ params })
+              WaterTestDetailsController.updateImage({ params })
               res.status(200).json({ certificateURL: url })
             }
           })
@@ -146,114 +147,3 @@ export const generateReport = async (req, res, next) => {
     }
   })
 }
-
-// export const upload = (req, res, next) => {
-//   var customOriginalName = "";
-//   var customPath = "";
-//   var customFieldName = "";
-//   var bucketName = "";
-//   var waterTestDetailsId = req.body.waterTestDetailsId;
-//   var description = req.body.description;
-
-//   aws.config.setPromisesDependency();
-//   aws.config.update({
-//     "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
-//     "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
-//   });
-
-//   if (req.files.floraFiles) {
-//     customFieldName = req.files.floraFiles[0].fieldname;
-//     customPath = req.files.floraFiles[0].path;
-//     customOriginalName = req.files.floraFiles[0].originalname;
-//     bucketName = "our-river-our-life-images/flora";
-
-//   }
-//   else if (req.files.faunaFiles) {
-//     customFieldName = req.files.faunaFiles[0].fieldname;
-//     customPath = req.files.faunaFiles[0].path;
-//     customOriginalName = req.files.faunaFiles[0].originalname;
-//     bucketName = "our-river-our-life-images/fauna";
-//   }
-//   else if (req.files.artworkFiles) {
-//     customFieldName = req.files.artwork[0].fieldname;
-//     customPath = req.files.artwork[0].path;
-//     customOriginalName = req.files.artwork[0].originalname;
-//     bucketName = "our-river-our-life-images/artwork";
-//   }
-//   else if (req.files.groupPicture) {
-//     customFieldName = req.files.groupPicture[0].fieldname;
-//     customPath = req.files.groupPicture[0].path;
-//     customOriginalName = req.files.groupPicture[0].originalname;
-//     bucketName = "our-river-our-life-images/groupPicture";
-//   }
-//   else if (req.files.activity) {
-//     customFieldName = req.files.activity[0].fieldname;
-//     customPath = req.files.activity[0].path;
-//     customOriginalName = req.files.activity[0].originalname;
-//     bucketName = "our-river-our-life-images/activity";
-//   }
-//   else if (req.files.river) {
-//     customFieldName = req.files.river[0].fieldname;
-//     customPath = req.files.river[0].path;
-//     customOriginalName = req.files.river[0].originalname;
-//     bucketName = "our-river-our-life-images/river";
-//   }
-
-//   const s3 = new aws.S3();
-//   var params = {
-//     ACL: 'public-read',
-//     Bucket: bucketName,
-//     Body: fs.createReadStream(customPath),
-//     Key: `${customOriginalName}`
-//   };
-
-//   s3.upload(params, (err, data) => {
-//     if (err) {
-//       console.log('Error occured while trying to upload to S3 bucket', err);
-//       res.status(500).send(err);
-//     }
-
-//     if (res) {
-//       var params = "";
-//       fs.unlinkSync(customPath); // Empty temp folder
-//       const locationUrl = data.Location;
-
-//       if (customFieldName == 'flora') {
-//         params = { "id": waterTestDetailsId, "flora": locationUrl, "fieldName": "flora", "description": description }
-//       }
-//       else if (customFieldName == 'fauna') {
-//         params = { "id": waterTestDetailsId, "fauna": locationUrl, "fieldName": "fauna", "description": description }
-//       }
-//       else if (customFieldName == 'artwork') {
-//         params = { "id": waterTestDetailsId, "artwork": locationUrl, "fieldName": "artwork", "description": description }
-//       }
-//       else if (customFieldName == 'groupPicture') {
-//         params = { "id": waterTestDetailsId, "groupPicture": locationUrl, "fieldName": "groupPicture", "description": description }
-//       }
-//       else if (customFieldName == 'activity') {
-//         params = { "id": waterTestDetailsId, "activity": locationUrl, "fieldName": "activity", "description": description }
-//       }
-//       else if (customFieldName == 'river') {
-//         params = { "id": waterTestDetailsId, "river": locationUrl, "fieldName": "river", "description": description }
-//       }
-//       if (params != "") {
-//         WaterTestDetailsController.updateImage({ params })
-//       }
-//       res.status(200).send("Image uploaded successfully");
-//     }
-//   });
-// }
-export const create = ({ body }, res, next) =>
-  res.status(201).json(body)
-
-export const index = ({ querymen: { query, select, cursor } }, res, next) =>
-  res.status(200).json([])
-
-export const show = ({ params }, res, next) =>
-  res.status(200).json({})
-
-export const update = ({ body, params }, res, next) =>
-  res.status(200).json(body)
-
-export const destroy = ({ params }, res, next) =>
-  res.status(204).end()
--- src/api/pdf/index.js
+++ src/api/pdf/index.js
@@ -1,6 +1,5 @@
 import { Router } from 'express'
-import { middleware as query } from 'querymen'
-import { create, index, show, update, destroy, generateReport } from './controller'
+import { generateReport } from './controller'
 
 const router = new Router()
 
@@ -14,60 +13,5 @@ const router = new Router()
  */
 router.post('/generateReport',
   generateReport)
-
-/**
- * @api {post} /pdf Create pdf
- * @apiName CreatePdf
- * @apiGroup Pdf
- * @apiSuccess {Object} pdf Pdf's data.
- * @apiError {Object} 400 Some parameters may contain invalid values.
- * @apiError 404 Pdf not found.
- */
-router.post('/',
-  create)
-
-/**
- * @api {get} /pdf Retrieve pdfs
- * @apiName RetrievePdfs
- * @apiGroup Pdf
- * @apiUse listParams
- * @apiSuccess {Object[]} pdfs List of pdfs.
- * @apiError {Object} 400 Some parameters may contain invalid values.
- */
-router.get('/',
-  query(),
-  index)
-
-/**
- * @api {get} /pdf/:id Retrieve pdf
- * @apiName RetrievePdf
- * @apiGroup Pdf
- * @apiSuccess {Object} pdf Pdf's data.
- * @apiError {Object} 400 Some parameters may contain invalid values.
- * @apiError 404 Pdf not found.
- */
-router.get('/:id',
-  show)
-
-/**
- * @api {put} /pdf/:id Update pdf
- * @apiName UpdatePdf
- * @apiGroup Pdf
- * @apiSuccess {Object} pdf Pdf's data.
- * @apiError {Object} 400 Some parameters may contain invalid values.
- * @apiError 404 Pdf not found.
- */
-router.put('/:id',
-  update)
-
-/**
- * @api {delete} /pdf/:id Delete pdf
- * @apiName DeletePdf
- * @apiGroup Pdf
- * @apiSuccess (Success 204) 204 No Content.
- * @apiError 404 Pdf not found.
- */
-router.delete('/:id',
-  destroy)
 
 export default router
