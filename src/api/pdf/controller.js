e8b7389df43c2dc2b2f21d7e815afc664c2be977
soujini <soujanya.venkatesh@gmail.com>
2023-07-03 14:27:03 +0530

    added platform folder

--- src/api/pdf/controller.js
+++ src/api/pdf/controller.js
@@ -184,42 +184,50 @@ export const generateReport = (req, res, next) => {
           res.send('Error in report template ' + err)
         } else {
           var test = 'Souju'
-          // res.send(test);
-          pdf.create(data, options).toBuffer(function (err, buffer) {
-            if (err) {
-              console.log('Error creating PDF: ' + err)
-              res.statusCode = 500
-              res.send(err)
-            } else {
-              var params = {
-                ACL: 'public-read',
-                Bucket: 'our-river-our-life-images/certificate',
-                Key: 'certificate_' + waterTestDetailsId,
-                Body: data,
-                ContentEncoding: 'buffer',
-                ContentType: 'application/pdf'
-              }
-              // res.type('application/pdf');
-              res.statusCode = 200
-              res.send({ data: buffer, success: true })
-              // res.status(200).json(params)
-              // res.send(params)
-              // res.send('ajksakjhdakjhdas3u27346')
-              // s3.upload(params, function (err, buffer) {
-              //   if (err) {
-              //     console.log(err)
-              //     console.log('Error uploading data: ', data)
-              //   } else {
-              //     // var certificateURL = data.Location
-              //     console.log('Succesfully uploaded pdf!')
-              //     var url = 'https://our-river-our-life-images.s3.amazonaws.com/certificate/certificate_' + waterTestDetailsId
-              //     params = { id: '649a71da9d819c140420bfaddd', certificate: url, fieldName: 'certificate' }
-              //     // WaterTestDetailsController.updateImage({ params })
-              //     res.status(200).json({ certificateURL: url })
-              //   }
-              // })
-            }
-          })
+          res.send(test);
+          // pdf
+          //   .create(data, options)
+          //   .then((res) => {
+          //     console.log(res)
+          //   })
+          //   .catch((error) => {
+          //     console.error(error)
+          //   })
+          // pdf.create(data, options).toBuffer(function (err, buffer) {
+          //   if (err) {
+          //     console.log('Error creating PDF: ' + err)
+          //     res.statusCode = 500
+          //     res.send(err)
+          //   } else {
+          //     var params = {
+          //       ACL: 'public-read',
+          //       Bucket: 'our-river-our-life-images/certificate',
+          //       Key: 'certificate_' + waterTestDetailsId,
+          //       Body: data,
+          //       ContentEncoding: 'buffer',
+          //       ContentType: 'application/pdf'
+          //     }
+          //     // res.type('application/pdf');
+          //     res.statusCode = 200
+          //     res.send({ data: buffer, success: true })
+          //     // res.status(200).json(params)
+          //     // res.send(params)
+          //     // res.send('ajksakjhdakjhdas3u27346')
+          //     // s3.upload(params, function (err, buffer) {
+          //     //   if (err) {
+          //     //     console.log(err)
+          //     //     console.log('Error uploading data: ', data)
+          //     //   } else {
+          //     //     // var certificateURL = data.Location
+          //     //     console.log('Succesfully uploaded pdf!')
+          //     //     var url = 'https://our-river-our-life-images.s3.amazonaws.com/certificate/certificate_' + waterTestDetailsId
+          //     //     params = { id: '649a71da9d819c140420bfaddd', certificate: url, fieldName: 'certificate' }
+          //     //     // WaterTestDetailsController.updateImage({ params })
+          //     //     res.status(200).json({ certificateURL: url })
+          //     //   }
+          //     // })
+          //   }
+          // })
         }
       })
     })
